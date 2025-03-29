35ae4daa5e419e257123e2f5112b5854b725e7ad
Zuriel Ali Sampé Sánchez <sampesanchezzurielali@gmail.com>
2025-03-28 23:45:27 -0600

    Add files via upload

---
+++ Playlist.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package playlist;
+
+/**
+ *
+ * @Zuriel Ali 
+ */
+public class Playlist {
+
+    public static void main(String[] args) {
+      Musicas P1=new Musicas("Reina","Trap Argentino","2018",4);
+      P1.ShowPlaylist();
+    }
+}
+     
